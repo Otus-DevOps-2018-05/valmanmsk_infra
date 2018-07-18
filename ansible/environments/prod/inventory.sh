eb0ca3aaf2da99605d48701801b36612d57291e8
Valeriy Mandarov <valman.ykt@gmail.com>
2018-07-18 11:09:19 -0500

    For push on Windows

--- ansible/environments/prod/inventory.sh
+++ ansible/environments/prod/inventory.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 SCRIPT_PATH=`dirname $0`
+cd ~/otus/valmanmsk_infra/terraform/prod
+app_ip=$(terraform output app_external_ip)
+db_ip=$(terraform output db_external_ip)
+cd ~/otus/valmanmsk_infra/ansible
+echo "{
+    "app": {
+        "hosts": [\"$app_ip\"]
+    },
+    "db": {
+        "hosts": [\"$db_ip\"]
+    }
+}
+" > $SCRIPT_PATH/inventory.json
+
 if [ "$1" == "--list" ];
 then
     cat $SCRIPT_PATH/inventory.json
